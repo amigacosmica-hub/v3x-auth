c788e9938dd8b6b0a57d53abff0a7b3d57fd78d3
amigacosmica-hub <melonfyimars@gmail.com>
2026-05-23 19:11:50 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,104 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect("mongodb+srv://amigacosmica:s5QWf1rCF1H9xrwO@cluster0.ncf8san.mongodb.net/?appName=Cluster0")
+.then(() => {
+    console.log("MongoDB Connected");
+})
+.catch((err) => {
+    console.log(err);
+});
+
+const LicenseSchema = new mongoose.Schema({
+    license: String,
+    hwid: String,
+    banned: Boolean
+});
+
+const License = mongoose.model("License", LicenseSchema);
+
+app.post("/validate", async (req, res) => {
+
+    const { license, hwid } = req.body;
+
+    const key = await License.findOne({ license });
+
+    if (!key)
+    {
+        return res.json({
+            success: false,
+            message: "Invalid license"
+        });
+    }
+
+    if (key.banned)
+    {
+        return res.json({
+            success: false,
+            message: "License banned"
+        });
+    }
+
+    if (!key.hwid)
+    {
+        key.hwid = hwid;
+        await key.save();
+    }
+
+    if (key.hwid !== hwid)
+    {
+        return res.json({
+            success: false,
+            message: "HWID mismatch"
+        });
+    }
+
+    return res.json({
+        success: true,
+        message: "License valid"
+    });
+
+});
+
+app.post("/create-license", async (req, res) => {
+
+    const { license } = req.body;
+
+    const existing = await License.findOne({ license });
+
+    if (existing)
+    {
+        return res.json({
+            success: false,
+            message: "License already exists"
+        });
+    }
+
+    const newLicense = new License({
+        license: license,
+        hwid: "",
+        banned: false
+    });
+
+    await newLicense.save();
+
+    return res.json({
+        success: true,
+        message: "License created"
+    });
+
+});
+
+app.get("/", (req, res) => {
+    res.send("V3X PANEL ONLINE");
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
